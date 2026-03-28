8db2899d2d210c2f5c330c6d2f252cebd24f62ff
gaurav <gauravkuntal393@gmail.com>
2026-03-28 12:10:46 +0530

    converted images to webp + responsive fixes + card fix

--- js/script.js
+++ js/script.js
@@ -65,3 +65,401 @@ window.addEventListener("load", function(){
   setInterval(changeSlide, 3000);
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const section = document.querySelector("#turnover");
+const fills = document.querySelectorAll(".fill");
+
+function showBars() {
+    const trigger = window.innerHeight * 0.8;
+    const sectionTop = section.getBoundingClientRect().top;
+
+    if (sectionTop < trigger) {
+        fills.forEach(fill => {
+            fill.style.width = fill.getAttribute("data-width");
+        });
+    }
+}
+
+window.addEventListener("scroll", showBars);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const silicone = document.querySelector("#silicone .silicone-container");
+
+function showSilicone() {
+    const trigger = window.innerHeight * 0.85;
+    const top = silicone.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        silicone.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showSilicone);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const japan = document.querySelector("#japan .japan-container");
+
+function showJapan() {
+    const trigger = window.innerHeight * 0.85;
+    const top = japan.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        japan.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showJapan);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const combo = document.querySelector("#combo");
+
+function showCombo() {
+    const trigger = window.innerHeight * 0.85;
+    const top = combo.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        combo.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showCombo);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const blue = document.querySelector("#blue");
+
+function showBlue() {
+    const trigger = window.innerHeight * 0.85;
+    const top = blue.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        blue.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showBlue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const energy = document.querySelector("#energy");
+
+function showEnergy() {
+    const trigger = window.innerHeight * 0.85;
+    const top = energy.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        energy.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showEnergy);
+
+
+
+
+
+
+
+
+
+
+
+const paint = document.querySelector("#paint");
+
+function showPaint() {
+    const trigger = window.innerHeight * 0.85;
+    const top = paint.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        paint.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showPaint);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const process = document.querySelector("#process");
+
+function showProcess() {
+    const trigger = window.innerHeight * 0.85;
+    const top = process.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        process.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showProcess);
+
+
+
+
+
+
+
+
+
+
+
+
+
+const assembly = document.querySelector("#assembly");
+
+function showAssembly() {
+    const trigger = window.innerHeight * 0.85;
+    const top = assembly.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        assembly.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showAssembly);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const keypad = document.querySelector("#keypad");
+const thumbs = document.querySelectorAll(".keypad-side img");
+const mainImg = document.querySelector(".keypad-main img");
+
+/* SCROLL ANIMATION */
+function showKeypad() {
+    const trigger = window.innerHeight * 0.85;
+    const top = keypad.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        keypad.classList.add("show");
+    }
+}
+window.addEventListener("scroll", showKeypad);
+
+/* IMAGE SWITCH */
+thumbs.forEach(img => {
+    img.addEventListener("click", () => {
+
+        mainImg.src = img.src;
+
+        thumbs.forEach(i => i.classList.remove("active"));
+        img.classList.add("active");
+
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const rubber = document.querySelector("#rubber");
+
+function showRubber() {
+    const trigger = window.innerHeight * 0.85;
+    const top = rubber.getBoundingClientRect().top;
+
+    if (top < trigger) {
+        rubber.classList.add("show");
+    }
+}
+
+window.addEventListener("scroll", showRubber);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(function(){
+
+    const spSection = document.getElementById("sp-section");
+
+    function spShowOnScroll() {
+        const trigger = window.innerHeight * 0.85;
+        const top = spSection.getBoundingClientRect().top;
+
+        if (top < trigger) {
+            spSection.classList.add("sp-show");
+        }
+    }
+
+    window.addEventListener("scroll", spShowOnScroll);
+
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+const certSection = document.querySelector(".cert-section");
+
+window.addEventListener("scroll", () => {
+  const top = certSection.getBoundingClientRect().top;
+
+  if (top < window.innerHeight - 100) {
+    certSection.classList.add("show");
+  }
+});
